ea53fa6e6a14fa1d659a8cc790a1f19c5841052e
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-06-19 13:20:03 +0530

    Tests with prime number condition added

--- number-filtering/number_filtering_test.go
+++ number-filtering/number_filtering_test.go
@@ -1,6 +1,7 @@
 package numberfiltering
 
 import (
+	"math"
 	"reflect"
 	"testing"
 )
@@ -12,6 +13,23 @@ var isOdd = func (num int) bool {
 
 var isEven = func (num int) bool {
     return num % 2 == 0
+}
+
+var isPrime = func(number int) bool {
+    if number <= 1 {
+        return false
+    } else if number == 2 {
+        return true
+    } else  {
+        isPrimeNumber:= true
+        for i:=2; float64(i) <= math.Sqrt(float64(number)); i++ {
+            if number % i == 0 {
+                isPrimeNumber = false
+                break
+            }
+        }
+        return isPrimeNumber
+    }
 }
 
 func TestGetOddNumbers(t *testing.T) {
@@ -53,6 +71,29 @@ func TestGetEvenNumbers(t *testing.T) {
 		numbers := []int{12, 77, 99, 60, 18, 93, 75, 49}
 		got:= FilterNumbersWithAllConditions(numbers, isEven)
 		want:= []int{12, 60, 18}
+
+		if !reflect.DeepEqual(got, want) {
+		t.Errorf("wanted: %v but got: %v", want, got)
+	}
+	})
+}
+
+func TestGetPrimeNumbers(t *testing.T) {
+
+	t.Run("Test get prime numbers with [1 2 3 4 5 6 7 8 9 10]", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		got:= FilterNumbersWithAllConditions(numbers, isPrime)
+		want:= []int{2, 3, 5, 7}
+
+		if !reflect.DeepEqual(got, want) {
+		t.Errorf("wanted: %v but got: %v", want, got)
+	}
+	})
+
+	t.Run("Test get prime numbers with [20 32 59 41 60 27 50 29 61 90]", func(t *testing.T) {
+		numbers := []int{20, 32, 59, 41, 60, 27, 50, 29, 61, 90}
+		got:= FilterNumbersWithAllConditions(numbers, isPrime)
+		want:= []int{59, 41, 29, 61}
 
 		if !reflect.DeepEqual(got, want) {
 		t.Errorf("wanted: %v but got: %v", want, got)
