e54a82b557a1aabae2afe2afdd84fa8775047702
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-06-19 13:16:04 +0530

    Tests with even number condition added

--- number-filtering/number_filtering_test.go
+++ number-filtering/number_filtering_test.go
@@ -10,6 +10,10 @@ var isOdd = func (num int) bool {
 	return num % 2 == 1
 }
 
+var isEven = func (num int) bool {
+    return num % 2 == 0
+}
+
 func TestGetOddNumbers(t *testing.T) {
 
 	t.Run("Test get odd numbers with [1 2 3 4 5 6 7 8 9 10]", func(t *testing.T) {
@@ -26,6 +30,29 @@ func TestGetOddNumbers(t *testing.T) {
 		numbers := []int{12, 77, 99, 60, 18, 93, 75, 49}
 		got:= FilterNumbersWithAllConditions(numbers, isOdd)
 		want:= []int{77, 99, 93, 75, 49}
+
+		if !reflect.DeepEqual(got, want) {
+		t.Errorf("wanted: %v but got: %v", want, got)
+	}
+	})
+}
+
+func TestGetEvenNumbers(t *testing.T) {
+
+	t.Run("Test get even numbers with [1 2 3 4 5 6 7 8 9 10]", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		got:= FilterNumbersWithAllConditions(numbers, isEven)
+		want:= []int{2, 4, 6, 8, 10}
+
+		if !reflect.DeepEqual(got, want) {
+		t.Errorf("wanted: %v but got: %v", want, got)
+	}
+	})
+
+	t.Run("Test get even numbers with [12 77 99 60 28 93 75 49]", func(t *testing.T) {
+		numbers := []int{12, 77, 99, 60, 18, 93, 75, 49}
+		got:= FilterNumbersWithAllConditions(numbers, isEven)
+		want:= []int{12, 60, 18}
 
 		if !reflect.DeepEqual(got, want) {
 		t.Errorf("wanted: %v but got: %v", want, got)
